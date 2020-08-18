3fad728ec1fedb22de52a8b2b78eee9e2efe8c94
ElectronApplications <36295932+ElectronApplications@users.noreply.github.com>
2020-08-18 16:09:58 +0800

    Add files via upload

--- src/examples/Gradient/Point.java
+++ src/examples/Gradient/Point.java
@@ -3,7 +3,7 @@ package examples.Gradient;
 public class Point {
     int x;
     int y;
-    int type; //0 - Red, 1 - Blue
+    int type; //0 - Red, 1 - Green, 2 - Blue
 
     Point(int x, int y, int type) {
         this.x = x;
