6660b9d00134733615fec9f100d6ca5ac22d00f5
ElectronApplications <36295932+ElectronApplications@users.noreply.github.com>
2020-08-18 14:44:23 +0800

    Add files via upload

---
+++ src/examples/Gradient/Point.java
@@ -0,0 +1,13 @@
+package examples.Gradient;
+
+public class Point {
+    int x;
+    int y;
+    int type;
+
+    Point(int x, int y, int type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+}
