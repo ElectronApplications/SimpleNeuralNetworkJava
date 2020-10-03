f9532134f949a3635bd19bcd160ce46ff8483bf8
ElectronApps <barbwirek4@gmail.com>
2020-10-03 21:17:41 +0800

    Added documentation comments

--- src/NeuralNetwork/Neuron.java
+++ src/NeuralNetwork/Neuron.java
@@ -3,6 +3,9 @@ package NeuralNetwork;
 public class Neuron {
     private double weights[];
 
+    /** 
+     * @param weights Amount of connections this neuron has with neurons on the next layer
+     */
     public Neuron(int weights) {
         this.weights = new double[weights];
 
@@ -11,14 +14,25 @@ public class Neuron {
         }
     }
 
+    /**
+     * @return Returns the amount of connections with the neurons on the next layer
+     */
     public int getWeightsAmount() {
         return weights.length;
     }
 
+    /**
+     * @param neuron The index of the connection
+     * @return Returns the weight of the connection
+     */
     public double getWeight(int neuron) {
         return weights[neuron];
     }
 
+    /**
+     * @param neuron The index of the connection
+     * @param weight Weight to set
+     */
     public void setWeight(int neuron, double weight) {
         weights[neuron] = weight;
     }
